bc6822fc9c55c8991cb5bbacddd75832ea8b6931
Mehdi Messarat <medimessarat@gmail.com>
2020-09-04 20:26:36 +0100

    define the structure of the project, implement dummy user & place pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+
+import NewPlace from "./places/pages/NewPlace";
+import Users from "./user/pages/Users";
 
 function App() {
   return (
-    <div>
-      <h1>Hi</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/places/newPlace" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/places/pages/NewPlace.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NewPlace extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to NewPlace</h1>
+      </div>
+    );
+  }
+}
+
+export default NewPlace;
