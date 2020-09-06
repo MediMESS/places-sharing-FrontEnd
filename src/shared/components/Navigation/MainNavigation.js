e8749921396c71fa66a42aaf8d17d5842dac00d7
Mehdi Messarat <medimessarat@gmail.com>
2020-09-06 17:44:36 +0100

    feat(SideDrawer): add SideDrawer for responsiveness when mobile screen to display navigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,24 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import NavLinks from "./NavLinks";
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
-const MainNavigation = () => {
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
+
+const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerOpen(true);
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerOpen(false);
+  };
+
+  console.log(drawerIsOpen);
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav>
+    <React.Fragment>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler} />}
+      <SideDrawer
+        show={drawerIsOpen}
+        onClick={closeDrawerHandler}
+        className="main-navigation__drawer-nav"
+      >
         <NavLinks />
-      </nav>
-    </MainHeader>
+      </SideDrawer>
+      <MainHeader>
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
