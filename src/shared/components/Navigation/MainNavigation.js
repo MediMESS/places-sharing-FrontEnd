2699818e75e9869f897ef34a494233c3510685ac
Mehdi Messarat <medimessarat@gmail.com>
2020-09-06 13:29:41 +0100

    style(navigation): incorporate links component in the MainNavigation

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import NavLinks from "./NavLinks";
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
 const MainNavigation = () => {
@@ -14,7 +15,9 @@ const MainNavigation = () => {
       <h1 className="main-navigation__title">
         <Link to="/">Your Places</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
