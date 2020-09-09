60872eb0eac98c4255de4bbb4df7d2b53f7d095f
Mehdi Messarat <medimessarat@gmail.com>
2020-09-09 14:31:59 +0100

    style: add the Button components which is the customized button / a tag

---
+++ src/shared/components/FormElements/Button.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Button.css";
+const Button = (props) => {
+  if (props.href) {
+    return (
+      <a
+        className={`button button--${props.size || "default"} ${
+          props.inverse && "button--inverse"
+        } ${props.danger && "button--danger"}`}
+        href={props.href}
+      >
+        {props.children}
+      </a>
+    );
+  } else if (props.to) {
+    return (
+      <Link
+        className={`button button--${props.size || "default"} ${
+          props.inverse && "button--inverse"
+        } ${props.danger && "button--danger"}`}
+        to={props.to}
+      >
+        {props.children}
+      </Link>
+    );
+  } else {
+    return (
+      <button
+        className={`button button--${props.size || "default"} ${
+          props.inverse && "button--inverse"
+        } ${props.danger && "button--danger"}`}
+        href={props.href}
+        onClick={props.onCLick}
+        disabled={props.disabled}
+        type={props.type}
+      >
+        {props.children}
+      </button>
+    );
+  }
+};
+
+export default Button;
