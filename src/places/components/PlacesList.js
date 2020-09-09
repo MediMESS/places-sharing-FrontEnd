8ccc7a38e5757b1be8eb122045574a73cbb9a474
Mehdi Messarat <medimessarat@gmail.com>
2020-09-09 14:28:56 +0100

    feat(userPlaces): Implement component for placesList which will be container for places item

--- src/places/components/PlacesList.js
+++ src/places/components/PlacesList.js
@@ -1,22 +1,7 @@
 import React from "react";
 
 import PlaceItem from "./PlaceItem";
-
-const places = [
-  {
-    id: "u1",
-    title="Bora Bora beach",
-    imgeUrl:
-      "https://www.wendyperrin.com/wp-content/uploads/2018/02/17-12-Brando-Suites-Bora-Bora-aerial-view-CR-Brando-Suites-1000x667.jpg",
-    description: "Very Beautiful #1 Place",
-    location: {
-      lat: 34,
-      long: 55,
-    },
-    creator: "u1",
-    address: "address place number #1",
-  },
-];
+import "./PlacesList.css";
 const PlacesList = (props) => {
   return (
     <ul className="places-list">
@@ -25,11 +10,11 @@ const PlacesList = (props) => {
           key={place.id}
           id={place.id}
           title={place.title}
-          imageUrl={imageUrl}
+          image={place.imageUrl}
           address={place.address}
           location={place.location}
           description={place.description}
-          creator={place.creator}
+          creatorId={place.creator}
         />
       ))}
     </ul>
