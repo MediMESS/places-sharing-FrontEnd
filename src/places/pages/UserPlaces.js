d53feffd5aa08ec21446658a8c27586529ec381b
Mehdi Messarat <medimessarat@gmail.com>
2020-09-11 21:37:44 +0100

    refactor(userPlaces): add import

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 import PlacesList from "../components/PlacesList";
-import Modal from "../../shared/components/UIElements/Modal";
 class UserPlaces extends Component {
   places = [
     {
