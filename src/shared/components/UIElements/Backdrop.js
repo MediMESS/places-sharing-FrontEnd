62a42f217da9944a039bab03c796f6987e90030f
Mehdi Messarat <medimessarat@gmail.com>
2020-09-12 23:56:55 +0100

    refactor: remove CSSTransition in Backdrop

--- src/shared/components/UIElements/Backdrop.js
+++ src/shared/components/UIElements/Backdrop.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CSSTransition from "react-transition-group";
 import "./Backdrop.css";
 const Backdrop = (props) => {
   return ReactDOM.createPortal(
