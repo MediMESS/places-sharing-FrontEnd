1c02bf849494a88c129012e1d60d0a04f2151c37
Mehdi Messarat <medimessarat@gmail.com>
2020-09-12 18:05:56 +0100

    feat(Users): add more dummy users

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -9,6 +9,13 @@ class Users extends Component {
         "https://i.insider.com/5e3b33270ce3d15c650b3ced?width=600&format=jpeg&auto=webp",
       places: 3,
     },
+    {
+      id: "u2",
+      name: "Jay-Z2",
+      image:
+        "https://i.insider.com/5e3b33270ce3d15c650b3ced?width=600&format=jpeg&auto=webp",
+      places: 3,
+    },
   ];
   render() {
     return <UsersList users={this.users} />;
