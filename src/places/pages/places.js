37f64cba981c45bb8a1f0239537d8430412fceb3
Mehdi Messarat <medimessarat@gmail.com>
2020-09-15 23:54:11 +0100

    refactor: put the data places inside one component

---
+++ src/places/pages/places.js
@@ -0,0 +1,57 @@
+export const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Bora Bora",
+    address: "Indonesia, Bora Bora",
+    description: " very #1 beautiful place",
+
+    location: {
+      longitude: 289.098,
+      latitude: 32.23,
+    },
+    imageUrl:
+      "https://i.pinimg.com/originals/94/b0/ca/94b0caaed79d45f4ded9178a5876d91e.jpg",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Kashmir valley",
+    address: "Pakistan takiminian",
+    description: " very #2 beautiful place",
+
+    location: {
+      longitude: 23.8,
+      latitude: -1221.12,
+    },
+    imageUrl:
+      "https://i.pinimg.com/originals/49/82/fe/4982fe7251384fcb452dd83329ecbc78.png",
+    creator: "u1",
+  },
+  {
+    id: "p3",
+    title: "Scandinave country",
+    description: " very #3 beautiful place",
+
+    address: "scandinavia",
+    location: {
+      longitude: -534.123,
+      latitude: -12.44,
+    },
+    imageUrl:
+      "https://www.etravel.com/eTravelImages/ArticleContentImage/0_39_97.jpg",
+    creator: "u1",
+  },
+  {
+    id: "p4",
+    title: "Scandinave country2",
+    description: " very #4 beautiful place",
+    address: "scandinavia2",
+    location: {
+      longitude: -534.123,
+      latitude: -12.44,
+    },
+    imageUrl:
+      "https://www.etravel.com/eTravelImages/ArticleContentImage/0_39_97.jpg",
+    creator: "u2",
+  },
+];
