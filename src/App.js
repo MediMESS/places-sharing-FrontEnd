634f6097238fb32fa930097a47f7e62b4cc63f5f
Mehdi Messarat <medimessarat@gmail.com>
2020-09-15 23:51:02 +0100

    feat(Auth): add the auth route

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import NewPlace from "./places/pages/NewPlace";
 import Users from "./user/pages/Users";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route path="/places/new" exact component={NewPlace} />
           <Route path="/places/:placeId" exact component={UpdatePlace} />
           <Route path="/:userId/places" exact component={UserPlaces} />
+          <Route path="/auth" exact component={Auth} />
           <Redirect to="/" />
         </Switch>
       </main>
