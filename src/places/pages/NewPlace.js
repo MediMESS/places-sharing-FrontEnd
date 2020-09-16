4d75f4a2732ce28df1debe6bb0255c6cbe00442d
Mehdi Messarat <medimessarat@gmail.com>
2020-09-16 23:57:56 +0100

    feat(NewPlace): implement new place form for userId

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,13 +1,74 @@
-import React, { Component } from "react";
+import React from "react";
+import useForm from "../../shared/hooks/form-hook";
 
-class NewPlace extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome to NewPlace</h1>
-      </div>
-    );
-  }
-}
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
+import "./FormPlace.css";
+import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+
+const NewPlace = () => {
+  const initialState = {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+      address: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  };
+
+  const [formState, inputHandler] = useForm(initialState);
+
+  const placeSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(formState.inputs);
+    // Send this to the BackEnd
+  };
+
+  return (
+    <form className="place-form" onSubmit={placeSubmitHandler}>
+      <h1 className="center">SHARE PLACE</h1>
+      <Input
+        id="title"
+        label="Title"
+        element="input"
+        type="text"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid title"
+        onInput={inputHandler}
+      />
+      <Input
+        id="description"
+        label="Description"
+        element="textarea"
+        validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description with minimum 5 characters"
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        label="Address"
+        element="input"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address"
+        onInput={inputHandler}
+      />
+      <Button type="submit" disabled={!formState.isValid}>
+        ADD PLACE
+      </Button>
+    </form>
+  );
+};
 
 export default NewPlace;
